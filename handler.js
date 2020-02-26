bb1bffe1923a47732bf42def55eed4cbe85ba258
Shawn Zimmerman <nwahsadude@users.noreply.github.com>
2020-02-26 17:34:07 -0600

    Added new endpoint

--- handler.js
+++ handler.js
@@ -12,7 +12,7 @@ module.exports.hello = async event => {
     },
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
+        message: "Go Serverless v1.0! Your function executed successfully! It did the thing",
         input: event
       },
       null,
@@ -23,3 +23,24 @@ module.exports.hello = async event => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+
+module.exports.new = async event => {
+  tagEvent("custom-tag", "hello new world", { custom: { tag: "data" } });
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+    },
+    body: JSON.stringify(
+        {
+          message: "This is a new thing",
+          input: event
+        },
+        null,
+        2
+    )
+  };
+
+}
